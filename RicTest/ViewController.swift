7bbffd24dc83b4bf2a0d6bc1070d350c806d2ebe
Frederick C. Lee <frederick.c.lee@gmail.com>
2021-09-29 14:05:06 -0700

    Initial Commit

---
+++ RicTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RicTest
+//
+//  Created by Frederick Lee on 9/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
